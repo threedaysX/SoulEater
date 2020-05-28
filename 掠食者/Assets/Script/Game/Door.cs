06693222f43fb6dc11ed0574c7727af609cc045a
HashBrown0707 <1qaz987@gmail.com>
2020-05-29 03:34:02 +0800

    no message

---
+++ 掠食者/Assets/Script/Game/Door.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    private Collider2D door;
+    [SerializeField] private Vector2 pointToTeleport;
+    [Header("把boss打開(SetActive)")]
+    [SerializeField] private GameObject boss;
+    private void Start()
+    {
+        door = GetComponent<Collider2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.gameObject.transform.position = pointToTeleport;
+            if(!boss.activeInHierarchy)
+                boss.SetActive(true);
+        }
+    }
+}
